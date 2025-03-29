7cfd45ae511c1b82d56153f1aea3b27a374b6d50
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-03-29 14:22:11 -0300

    implement main quiz logic with progress tracking

--- buzzfeed-quiz/src/app/quiz/quiz.component.ts
+++ buzzfeed-quiz/src/app/quiz/quiz.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+import { QuizService } from '../quiz.service';
+import { Question, QuizResult } from '../models';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.scss'
+  styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit { 
+  questions: Question[] = []; 
+  currentQuestionIndex = 0;
+  showResult = false;
+  result!: QuizResult;
 
+  constructor(private quizService: QuizService) {}
+
+  ngOnInit(): void { 
+    this.questions = this.quizService.getQuestions(); 
+  }
+
+  onAnswerSelected(score: number): void {
+    this.quizService.addToScore(score);
+    
+    if(this.currentQuestionIndex < this.questions.length - 1) {
+      this.currentQuestionIndex++;
+    } else {
+      this.showResult = true;
+      this.result = this.quizService.getFinalResult();
+    }
+  }
+
+  restartQuiz(): void {
+    this.quizService.resetQuiz();
+    this.currentQuestionIndex = 0;
+    this.showResult = false;
+  }
 }
