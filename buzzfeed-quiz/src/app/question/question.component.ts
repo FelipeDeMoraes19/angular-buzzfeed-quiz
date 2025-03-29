d7909013aa9cff1714d04de9e71e732f1e1e3a73
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-03-29 14:16:13 -0300

    implement question component with answer selection

--- buzzfeed-quiz/src/app/question/question.component.ts
+++ buzzfeed-quiz/src/app/question/question.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Question, Answer } from '../models';
 
 @Component({
   selector: 'app-question',
-  imports: [],
   templateUrl: './question.component.html',
-  styleUrl: './question.component.scss'
+  styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent {
+  @Input() question!: Question;
+  @Output() answerSelected = new EventEmitter<number>();
 
+  selectAnswer(answer: Answer): void {
+    this.answerSelected.emit(answer.score);
+  }
 }
